674990ce56c6637e5fb2cb00079e212243453cc3
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2024-03-02 11:09:45 +0530

    doneee

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let randomNumber; 
-let remainingAttempts = 3; 
+let randomNumber;
+let remainingAttempts = 3;
 const inputNum = document.getElementById('num');
 const resultElement = document.getElementById('result');
 
@@ -10,7 +10,8 @@ function guess() {
     }
 
     if (!randomNumber) {
-        randomNumber = Math.round(Math.random() * 100);
+        // Generate a random number between 1 and 20
+        randomNumber = Math.floor(Math.random() * 20) + 1;
         console.log("Random Number:", randomNumber);
     }
 
@@ -18,20 +19,23 @@ function guess() {
 
     remainingAttempts--;
 
-    if (userGuess === randomNumber) {
+    if (userGuess > 20) {
+        alert("You can't use numbers above 20.");
+        remainingAttempts++; // Increase remaining attempts since user entered an invalid number
+    } else if (userGuess === randomNumber) {
         resultElement.textContent = "You win!";
         remainingAttempts = 0; // Reset remaining attempts
     } else if (userGuess > randomNumber) {
         resultElement.textContent = "The needed one is smaller. Remaining attempts: " + remainingAttempts;
     } else if (userGuess < randomNumber) {
         resultElement.textContent = "The needed one is larger. Remaining attempts: " + remainingAttempts;
-        
     }
 
     if (remainingAttempts === 0) {
-        resultElement.textContent += " You have used all your attempts!";
+        resultElement.textContent += " You have used all your attempts! The number was " + randomNumber + ".";
         setTimeout(function() {
             window.location.reload();
-        }, 3000);
+        }, 5000);
     }
+    
 }
